e6bbd2e52721588409b314c760ca889af4382925
chibuzo <chibuzo.henry@gmail.com>
2024-09-09 11:46:17 +0100

    Improve admin ui

--- routes/property.js
+++ routes/property.js
@@ -22,18 +22,8 @@ router.get('/new', async (req, res, next) => {
 router.post('/new', async (req, res, next) => {
     try {
         const property = await propertyService.create(req.body);
-        res.redirect(`/property/list`);
+        res.redirect(`/property/${property.id}/edit`);
     } catch (err) {
-        next(err);
-    }
-});
-
-router.get('/:id/:title', async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const property = await propertyService.view(id);
-        res.render('property', { property });
-    } catch(err) {
         next(err);
     }
 });
@@ -53,7 +43,7 @@ router.post('/:id/update', async (req, res, next) => {
     try {
         const { id } = req.params;
         await propertyService.uodate(id, req.body);
-        res.redirect(`/property/:${id}`);
+        res.redirect(`/property/${id}/edit`);
     } catch(err) {
         next(err);
     }
@@ -63,8 +53,18 @@ router.post('/:id/upload-photos', async (req, res, next) => {
     try {
         const { id: propertyId } = req.params;
         await propertyService.uploadPropertyPhotos(propertyId, req.files);
-        res.redirect(`/property/${propertyId}`);
+        res.redirect(`/property/${propertyId}/edit`);
     } catch (err) {
+        next(err);
+    }
+});
+
+router.get('/:id/:title', async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const property = await propertyService.view(id);
+        res.render('property', { property });
+    } catch(err) {
         next(err);
     }
 });
