0c85aafb191de442dfd364719b33d2d89e0b7280
Chibuzo <chibuzo.henry@gmail.com>
2026-03-27 07:59:12 +0100

    Fix slider, add sticky nav

--- public/js/main.js
+++ public/js/main.js
@@ -198,6 +198,34 @@
          preloader: false,
      });
  
- 
+    if ($('.how-slider').length) {
+        $('.how-slider').owlCarousel({
+            loop: true,
+            margin: 30,
+            nav: true,
+            dots: true,
+            autoplay: true,
+            navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+            responsive: {
+                0: {
+                    items: 1
+                },
+                768: {
+                    items: 2
+                },
+                992: {
+                    items: 3
+                }
+            }
+        });
+    }
+
+    $(window).on('scroll', function() {
+        if ($(window).scrollTop() > 100) {
+            $('.nav-section').addClass('sticky');
+        } else {
+            $('.nav-section').removeClass('sticky');
+        }
+    });
+
  })(jQuery);
- 
